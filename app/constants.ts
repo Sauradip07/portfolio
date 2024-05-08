ad349f9c97dc8f2a541bc1fe57e48a913576142d
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-05-08 21:42:08 +0530

    project s added

--- app/constants.ts
+++ app/constants.ts
@@ -4,7 +4,7 @@ export const NavbarUrls = {
 	ABOUT: { name: 'About', url: '/about' },
 	EXPERIENCES: { name: 'Experiences', url: '/experiences' },
 	SKILLS: { name: 'Skills', url: '/skills' },
-	PORTFOLIO: { name: 'Project', url: '/portfolio' },
+	PORTFOLIO: { name: 'Projects', url: '/portfolio' },
 	CONTACT: { name: 'Contact', url: '/contact' },
 };
 
